bfb1c4fa6208330a930729463cb7414102160603
Dmitriy Plotitsyn <plhome@yandex.ru>
2022-07-16 22:09:58 +0300

    task6

--- src/ts/task6.ts
+++ src/ts/task6.ts
@@ -1,3 +1,8 @@
+// Задача состоит в том что написать калькулято для натуральных чисел, но только на типах!
+// Ниже приведена заготовка
+// Хочется поддержки сложения и вычитания, если хочется еще челленджа, то деление и умножение
+// Из-за ограничений глубины вычислений поддержать все натуральные числа не получится, это нормально
+
 type Increase< A, HELPER extends Array<number> = [] > =
   HELPER['length'] extends A
   ? [...HELPER, 0]['length']
@@ -13,5 +18,7 @@ type Subtract<A, B> = B extends 0 ? A : Subtract<Decrease<A>, Decrease<B>>;
 
 type Equals<A, B> = A extends B ? (B extends A ? "success" : never) : never;
 
-export type OnePlusOneTest = Equals<Add<1, 1>, 2>;
+export type OnePlusOneTest  = Equals<Add<1, 1>, 2>;
 export type TwoMinusOneTest = Equals<Subtract<2, 1>, 1>;
+export type Test_res7       = Equals<Subtract<10, 3>, 7>;
+export type Test_res13      = Equals<Add<10, 3>, 13>;

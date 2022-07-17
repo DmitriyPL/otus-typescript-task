4ec3e906536ede15c4d065aae1b2ae591743fa28
Dmitriy Plotitsyn <plhome@yandex.ru>
2022-07-17 16:31:56 +0300

    add test task6

---
+++ src/ts/task6.test.ts
@@ -0,0 +1,25 @@
+import { OnePlusOneTest, TwoMinusOneTest, Test_res7, Test_res13 } from "./task6";
+
+describe("function getOrderState", () => {
+
+  it("OnePlusOneTest", () => {
+    let res1: OnePlusOneTest = "success";
+    expect(res1).toBe("success");
+  });
+
+  it("TwoMinusOneTest'", () => {
+    let res2: TwoMinusOneTest = "success";
+    expect(res2).toBe("success");
+  });
+
+  it("Test_res7'", () => {
+    let res3: Test_res7 = "success";
+    expect(res3).toBe("success");
+  });
+
+  it("Test_res13'", () => {
+    let res4: Test_res13 = "success";
+    expect(res4).toBe("success");
+  });  
+
+});
